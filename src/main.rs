8e7f8c7e7e100f50554d986d465f74da4726bf43
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-02 07:49:01 -0600

    did it again, rewritten :)

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
+
 use std::io;
 use std::fs;
+use std::io::Error;
 use std::process::exit;
 
-fn write_to_file() -> io::Result<String> {
+fn write_to_file() -> Result<String, Error> {
     let input = io::stdin();
 
     println!("What file would you like to write to?");
     let mut x: String = String::new();
     input.read_line(&mut x)?;
 
-    print!("What would you like to write to the file?");
+    println!("What would you like to write to the file?");
     let mut y: String = String::new();
     input.read_line(&mut y)?;
 
-    fs::write(x.trim(), y.trim())?;
+    fs::write(&x.trim(), y.trim())?;
 
     Ok(x)
 }
 
 fn main() {
     match write_to_file() {
-        Ok(some) => println!("Successfully wrote to the file {some}"),
+        Ok(some) => println!("Successfully wrote to file {some}"),
         Err(error) => {
-            eprintln!("There was an error: {error}");
-            exit(1);
+            eprintln!("There was an error {error}");
+            exit(1)
+        }
     }
-};
 }
